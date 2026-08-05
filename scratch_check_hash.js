f258c4071bfac99c35cbe9026750fe6a2409bf9c
hemvantyadav-sys <hemvant.yadav@assrm.edu.in>
2026-08-05 21:29:54 +0530

    clean backend unneeded files

--- scratch_check_hash.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require('bcrypt');
-async function main() {
-  console.log(await bcrypt.compare('Admin@123', '$2b$10$Cyi8Vft/3MCUtTF7Oy1d7Ohua1u/To3u9zxbBq0M89NMMSQfCLaK.'));
-}
-main();
